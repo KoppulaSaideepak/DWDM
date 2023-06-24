a0d1bebcc9d47aa3add4441109acf80e8019b549
KoppulaSaideepak <112766191+KoppulaSaideepak@users.noreply.github.com>
2023-06-24 22:41:10 +0530

    Create scatterplot.R

---
+++ scatterplot.R
@@ -0,0 +1,17 @@
+input <- mtcars[,c('wt','mpg')]
+png(file = "scatterplot.png")
+plot(x = input$wt,y = input$mpg,
+     xlab = "Weight",
+     ylab = "Milage",
+     xlim = c(2.5,5),
+     ylim = c(15,30),		 
+     main = "Weight vs Milage")
+dev.off()
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
